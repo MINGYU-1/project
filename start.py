f50b9a25a5b00c35b7144672fc11385e7773bcde
michael998712 <michael998712@gmail.com>
2025-09-12 23:08:15 +0900

    2025-09-12

---
+++ start.py
@@ -0,0 +1,67 @@
+
+
+## streamlit 관련 모듈 불러오기
+import streamlit as st
+from streamlit.runtime.uploaded_file_manager import UploadedFile
+
+
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_core.documents.base import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain.prompts import PromptTemplate
+from langchain_core.runnables import Runnable
+from langchain.schema.output_parser import StrOutputParser
+from langchain_community.document_loaders import PyMuPDFLoader
+from typing import List
+import os
+import fitz  # PyMuPDF
+import re
+
+## 환경변수 불러오기
+from dotenv import load_dotenv,dotenv_values
+load_dotenv()
+
+
+############################### 1단계 : PDF 문서를 벡터DB에 저장하는 함수들 ##########################
+
+## 1: 임시폴더에 파일 저장
+def save_uploadedfile(uploadedfile: UploadedFile) -> str : 
+    temp_dir = "PDF_임시폴더"
+    if not os.path.exists(temp_dir):
+        os.makedirs(temp_dir)
+    file_path = os.path.join(temp_dir, uploadedfile.name)
+    with open(file_path, "wb") as f:
+        f.write(uploadedfile.read()) 
+    return file_path
+
+## 2: 저장된 PDF 파일을 Document로 변환
+def pdf_to_documents(pdf_path:str) -> List[Document]:
+    documents = []
+    loader = PyMuPDFLoader(pdf_path)
+    doc = loader.load()
+    for d in doc:
+        d.metadata['file_path'] = pdf_path
+    documents.extend(doc)
+    return documents
+
+## 3: Document를 더 작은 document로 변환
+def chunk_documents(documents: List[Document]) -> List[Document]:
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=800, chunk_overlap=100)
+    return text_splitter.split_documents(documents)
+
+## 4: Document를 벡터DB로 저장
+def save_to_vector_store(documents: List[Document]) -> None:
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+    vector_store = FAISS.from_documents(documents, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+def main():
+    pdf_doc = st.file_uploader("PDF Uploader",type = 'pdf')
+    button = st.button('pdf 업로드 하기')
+    if pdf_doc and button:
+        st.text("여기까지 구현됨")
+        pdf_path = save_uploadedfile(pdf_doc)
+        pdf_documents = pdf_to_documents(pdf_path)
+        smaller_documents = chunk_documents(pdf_documents)
+        save_to_vector_store(smaller_documents)
